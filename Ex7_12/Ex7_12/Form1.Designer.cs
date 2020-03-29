58391b6b6aaad4b3deac0278f9d0629f0320a8fe
denglun0302 <2393751439@qq.com>
2020-03-29 14:52:23 +0800

    提交代码

---
+++ Ex7_12/Ex7_12/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace Ex7_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnOrange = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnControl = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLine5 = new System.Windows.Forms.Button();
+            this.btnLine3 = new System.Windows.Forms.Button();
+            this.btnLine2 = new System.Windows.Forms.Button();
+            this.btnLine4 = new System.Windows.Forms.Button();
+            this.btnLine1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnca = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(451, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 544);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnBlue);
+            this.groupBox3.Controls.Add(this.btnBlack);
+            this.groupBox3.Controls.Add(this.btnOrange);
+            this.groupBox3.Controls.Add(this.btnGreen);
+            this.groupBox3.Controls.Add(this.btnYellow);
+            this.groupBox3.Controls.Add(this.btnRed);
+            this.groupBox3.Controls.Add(this.btnControl);
+            this.groupBox3.Location = new System.Drawing.Point(26, 379);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(158, 143);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlue.Location = new System.Drawing.Point(106, 89);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(27, 23);
+            this.btnBlue.TabIndex = 6;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.BackColor = System.Drawing.Color.Black;
+            this.btnBlack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlack.Location = new System.Drawing.Point(58, 89);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(27, 23);
+            this.btnBlack.TabIndex = 5;
+            this.btnBlack.UseVisualStyleBackColor = false;
+            // 
+            // btnOrange
+            // 
+            this.btnOrange.BackColor = System.Drawing.Color.Orange;
+            this.btnOrange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrange.Location = new System.Drawing.Point(9, 89);
+            this.btnOrange.Name = "btnOrange";
+            this.btnOrange.Size = new System.Drawing.Size(27, 23);
+            this.btnOrange.TabIndex = 4;
+            this.btnOrange.UseVisualStyleBackColor = false;
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Green;
+            this.btnGreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGreen.Location = new System.Drawing.Point(106, 53);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(27, 23);
+            this.btnGreen.TabIndex = 3;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYellow.Location = new System.Drawing.Point(58, 53);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(27, 23);
+            this.btnYellow.TabIndex = 2;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRed.Location = new System.Drawing.Point(6, 51);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(30, 23);
+            this.btnRed.TabIndex = 1;
+            this.btnRed.UseVisualStyleBackColor = false;
+            // 
+            // btnControl
+            // 
+            this.btnControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnControl.Location = new System.Drawing.Point(58, 24);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(27, 23);
+            this.btnControl.TabIndex = 0;
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            this.btnControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnControl_MouseDown);
+            this.btnControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnControl_MouseMove);
+            this.btnControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnControl_MouseUp);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLine5);
+            this.groupBox2.Controls.Add(this.btnLine3);
+            this.groupBox2.Controls.Add(this.btnLine2);
+            this.groupBox2.Controls.Add(this.btnLine4);
+            this.groupBox2.Controls.Add(this.btnLine1);
+            this.groupBox2.Location = new System.Drawing.Point(21, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(158, 143);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // btnLine5
+            // 
+            this.btnLine5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLine5.Image = ((System.Drawing.Image)(resources.GetObject("btnLine5.Image")));
+            this.btnLine5.Location = new System.Drawing.Point(5, 101);
+            this.btnLine5.Name = "btnLine5";
+            this.btnLine5.Size = new System.Drawing.Size(110, 13);
+            this.btnLine5.TabIndex = 4;
+            this.btnLine5.Text = "button1";
+            this.btnLine5.UseVisualStyleBackColor = true;
+            this.btnLine5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLine3
+            // 
+            this.btnLine3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLine3.Image = ((System.Drawing.Image)(resources.GetObject("btnLine3.Image")));
+            this.btnLine3.Location = new System.Drawing.Point(5, 63);
+            this.btnLine3.Name = "btnLine3";
+            this.btnLine3.Size = new System.Drawing.Size(110, 13);
+            this.btnLine3.TabIndex = 3;
+            this.btnLine3.Text = "button1";
+            this.btnLine3.UseVisualStyleBackColor = true;
+            this.btnLine3.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnLine2
+            // 
+            this.btnLine2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLine2.Image = ((System.Drawing.Image)(resources.GetObject("btnLine2.Image")));
+            this.btnLine2.Location = new System.Drawing.Point(7, 44);
+            this.btnLine2.Name = "btnLine2";
+            this.btnLine2.Size = new System.Drawing.Size(110, 13);
+            this.btnLine2.TabIndex = 2;
+            this.btnLine2.Text = "button1";
+            this.btnLine2.UseVisualStyleBackColor = true;
+            this.btnLine2.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnLine4
+            // 
+            this.btnLine4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLine4.Image = ((System.Drawing.Image)(resources.GetObject("btnLine4.Image")));
+            this.btnLine4.Location = new System.Drawing.Point(6, 82);
+            this.btnLine4.Name = "btnLine4";
+            this.btnLine4.Size = new System.Drawing.Size(110, 13);
+            this.btnLine4.TabIndex = 1;
+            this.btnLine4.Text = "button6";
+            this.btnLine4.UseVisualStyleBackColor = true;
+            this.btnLine4.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnLine1
+            // 
+            this.btnLine1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLine1.Image = ((System.Drawing.Image)(resources.GetObject("btnLine1.Image")));
+            this.btnLine1.Location = new System.Drawing.Point(7, 25);
+            this.btnLine1.Name = "btnLine1";
+            this.btnLine1.Size = new System.Drawing.Size(110, 13);
+            this.btnLine1.TabIndex = 0;
+            this.btnLine1.Text = "button1";
+            this.btnLine1.UseVisualStyleBackColor = true;
+            this.btnLine1.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnca);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.btn);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.btnLine);
+            this.groupBox1.Controls.Add(this.btnPen);
+            this.groupBox1.Location = new System.Drawing.Point(19, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(158, 143);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnca
+            // 
+            this.btnca.Image = ((System.Drawing.Image)(resources.GetObject("btnca.Image")));
+            this.btnca.Location = new System.Drawing.Point(117, 83);
+            this.btnca.Name = "btnca";
+            this.btnca.Size = new System.Drawing.Size(26, 23);
+            this.btnca.TabIndex = 5;
+            this.btnca.Tag = "1";
+            this.btnca.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(43, 83);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 4;
+            this.button12.Text = "";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Image = ((System.Drawing.Image)(resources.GetObject("btn.Image")));
+            this.btn.Location = new System.Drawing.Point(117, 54);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(23, 23);
+            this.btn.TabIndex = 3;
+            this.btn.Tag = "1";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(65, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnLine
+            // 
+            this.btnLine.Image = ((System.Drawing.Image)(resources.GetObject("btnLine.Image")));
+            this.btnLine.Location = new System.Drawing.Point(117, 24);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(23, 23);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.UseVisualStyleBackColor = true;
+            // 
+            // btnPen
+            // 
+            this.btnPen.Image = ((System.Drawing.Image)(resources.GetObject("btnPen.Image")));
+            this.btnPen.Location = new System.Drawing.Point(65, 24);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(29, 23);
+            this.btnPen.TabIndex = 0;
+            this.btnPen.Tag = "1";
+            this.btnPen.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(445, 544);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(445, 522);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(6, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 0);
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 544);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "画图";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLine5;
+        private System.Windows.Forms.Button btnLine3;
+        private System.Windows.Forms.Button btnLine2;
+        private System.Windows.Forms.Button btnLine4;
+        private System.Windows.Forms.Button btnLine1;
+        private System.Windows.Forms.Button btnca;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnOrange;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+    }
+}
+
---
+++ Ex7_12/Ex7_12/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex7_12
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g;
+        Point pStart, pEnd;     	// 定义画图的起始终点
+        int ChoiceGraph;        	// 所选择图形枚举
+        int penWidth;           	// 画笔宽度
+        enum mySelected
+        {
+            Pencil,          	// 铅笔
+            Line,            	// 直线
+            Ellipse,         	// 空心椭圆
+            FillEllipse,       	// 填充椭圆
+            Rec,             	// 空心矩形
+            FillRec,         	// 填充矩形
+            Eraser          	// 橡皮擦
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                  g = this.pictureBox1.CreateGraphics();         ChoiceGraph = (int)mySelected.Pencil;    		// 默认选择为铅笔工具         penWidth = 3;          	 			// 初始化画笔宽度          btnControl.BackColor = btnBlack.BackColor;  	// 默认黑色笔
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnControl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //把所有按钮的背景色都设为 White
+            btnLine1.BackColor = Color.White;
+            btnLine2.BackColor = Color.White;
+            btnLine3.BackColor = Color.White;
+            btnLine4.BackColor = Color.White;
+            btnLine5.BackColor = Color.White;
+            ((Button)sender).BackColor = Color.Black;          // 选中的按钮背景色为黑色
+            penWidth = Convert.ToInt32(((Button)sender).Tag);   // 将宽度按钮的 Tag 值设为画笔宽度
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //把所有按钮的背景色都设为 White
+            btnLine1.BackColor = Color.White;
+            btnLine2.BackColor = Color.White;
+            btnLine3.BackColor = Color.White;
+            btnLine4.BackColor = Color.White;
+            btnLine5.BackColor = Color.White;
+            ((Button)sender).BackColor = Color.Black;          // 选中的按钮背景色为黑色
+            penWidth = Convert.ToInt32(((Button)sender).Tag);   // 将宽度按钮的 Tag 值设为画笔宽度
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //把所有按钮的背景色都设为 White
+            btnLine1.BackColor = Color.White;
+            btnLine2.BackColor = Color.White;
+            btnLine3.BackColor = Color.White;
+            btnLine4.BackColor = Color.White;
+            btnLine5.BackColor = Color.White;
+            ((Button)sender).BackColor = Color.Black;          // 选中的按钮背景色为黑色
+            penWidth = Convert.ToInt32(((Button)sender).Tag);   // 将宽度按钮的 Tag 值设为画笔宽度
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //把所有按钮的背景色都设为 White
+            btnLine1.BackColor = Color.White;
+            btnLine2.BackColor = Color.White;
+            btnLine3.BackColor = Color.White;
+            btnLine4.BackColor = Color.White;
+            btnLine5.BackColor = Color.White;
+            ((Button)sender).BackColor = Color.Black;          // 选中的按钮背景色为黑色
+            penWidth = Convert.ToInt32(((Button)sender).Tag);   // 将宽度按钮的 Tag 值设为画笔宽度
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //把所有按钮的背景色都设为 White
+            btnLine1.BackColor = Color.White;
+            btnLine2.BackColor = Color.White;
+            btnLine3.BackColor = Color.White;
+            btnLine4.BackColor = Color.White;
+            btnLine5.BackColor = Color.White;
+            ((Button)sender).BackColor = Color.Black;          // 选中的按钮背景色为黑色
+            penWidth = Convert.ToInt32(((Button)sender).Tag);   // 将宽度按钮的 Tag 值设为画笔宽度
+        }
+
+        private void btnControl_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnControl_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnControl_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
