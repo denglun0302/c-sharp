facdf5ca6c99d4a41a73ee5272d4610d9cd55a37
denglun0302 <2393751439@qq.com>
2020-04-01 18:05:14 +0800

    提交代码

---
+++ Ex8_5/Ex8_5/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Ex8_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStreamWrite = new System.Windows.Forms.Button();
+            this.btnStresmRead = new System.Windows.Forms.Button();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStreamWrite
+            // 
+            this.btnStreamWrite.Location = new System.Drawing.Point(12, 218);
+            this.btnStreamWrite.Name = "btnStreamWrite";
+            this.btnStreamWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnStreamWrite.TabIndex = 0;
+            this.btnStreamWrite.Text = "文本保存";
+            this.btnStreamWrite.UseVisualStyleBackColor = true;
+            this.btnStreamWrite.Click += new System.EventHandler(this.btnStreamWrite_Click);
+            // 
+            // btnStresmRead
+            // 
+            this.btnStresmRead.Location = new System.Drawing.Point(93, 218);
+            this.btnStresmRead.Name = "btnStresmRead";
+            this.btnStresmRead.Size = new System.Drawing.Size(75, 23);
+            this.btnStresmRead.TabIndex = 1;
+            this.btnStresmRead.Text = "文本读取";
+            this.btnStresmRead.UseVisualStyleBackColor = true;
+            this.btnStresmRead.Click += new System.EventHandler(this.btnStresmRead_Click);
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.AutoSize = true;
+            this.btnBinaryWrite.Location = new System.Drawing.Point(174, 218);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(92, 25);
+            this.btnBinaryWrite.TabIndex = 2;
+            this.btnBinaryWrite.Text = "二进制保存";
+            this.btnBinaryWrite.UseVisualStyleBackColor = true;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.Location = new System.Drawing.Point(285, 220);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(94, 23);
+            this.btnBinaryRead.TabIndex = 3;
+            this.btnBinaryRead.Text = "二进制读取";
+            this.btnBinaryRead.UseVisualStyleBackColor = true;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(325, 171);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 253);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.btnStresmRead);
+            this.Controls.Add(this.btnStreamWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStreamWrite;
+        private System.Windows.Forms.Button btnStresmRead;
+        private System.Windows.Forms.Button btnBinaryWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
