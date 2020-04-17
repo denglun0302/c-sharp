94b59e273a06d5805ea61234ba05be293343872d
denglun0302 <2393751439@qq.com>
2020-04-17 14:01:50 +0800

    提交代码

---
+++ Project5/Project5/test.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+#include<windows.h>
+#include<time.h>
+#include<stdlib.h>
+#pragma warning(disable:4996)
+int main()
+{   
+	int n = 0;
+	printf("%d", n);
+	/*int a = 0;
+	int n = 0;
+	scanf("%d", &a);
+	scanf("%d", &n);
+
+	int sum = 0;
+	int temp = 0;
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		temp = 10 * temp + a;
+		sum = sum + temp;
+	}
+	printf("%d", sum);*/
+
+	/*int i = 0;
+	
+	int sum=0;
+	for (i = 100; i <=200; i++)
+	{
+		int count = 1;
+		int temp = i;
+		while (temp/ 10！ = 0)
+		{
+			count++;
+			temp= temp/ 10;
+		}
+		temp = i;
+		while (temp != 0)
+		{
+		
+			sum = sum + pow((double）（temp % 10）, count);
+			temp = temp / 10;
+		}
+		if（sum == i）
+		{
+			printf("%d", i);
+		}
+           
+	}*/
+	//1.打断点，f9
+	//f10   单步调试  逐过程调试   
+	//f11   进入函数   逐语句调试
+
+	//int line = 7;
+	//int i = 0;
+	//for (i = 0; i < line; i++)
+	//{
+	//	int j = 0;
+	//	for (; j < line - i - 1; j++)//line-1
+	//	{
+	//		printf(" ");
+	//	}
+	//	for (j = 0; j < 2 * i + 1; j++)//2*(line-1-i)-1
+	//	{
+	//		printf("*");
+
+	//	}
+	//	printf("\n");
+	//}
+	return 0;
+	system("pause");
+}
