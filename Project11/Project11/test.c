7902b2cf1dcc755c7a4867beeb4492c49be76248
denglun0302 <2393751439@qq.com>
2020-05-14 20:44:26 +0800

    提交代码

---
+++ Project11/Project11/test.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<windows.h>
+#pragma warning (disable:4996)
+
+
+//int diffbit(int m, int n)
+//{
+//	int temp = 0;
+//	int count = 0;
+//	temp = m^n;
+//	while (temp)
+//	{
+//		temp = temp&(temp - 1);
+//		count++;
+//	}
+//	return count;
+//}
+int main()
+{
+	
+//	
+//	int m = 445;
+//	int n = 447;
+//	int num = diffbit(m, n);
+//	printf("%d", num);
+	/*int n = 0;
+	int m = 0;
+	int i = 0;
+	int a[4];
+	int len = sizeof(a) / sizeof(a[0]);
+
+	scanf("%d", &n);
+	while (n>0)
+	{
+		m = n % 10;
+		n /= 10;
+		a[i] = m;
+		i++;
+	}
+	exchange(a, len);
+	for (i = 0; i<len; i++)
+	{
+		printf("%d\n", a[i]);
+	}*/
+	system("pause");
+	return 0;
+}
